7563ef85a343fba74ffafcb52459a2b84e29e0e5
Jose Patino <j.edgarpatino11@gmail.com>
2026-01-14 13:40:36 -0600

    Set up alembic for database migration

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     database_url: str
@@ -8,4 +8,4 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
 
-Settings = Settings()
+settings = Settings()
